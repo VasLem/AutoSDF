e82d5a92e5b990fe652cd12595f42cacfda297be
vaslem <vaslemonidis@hotmail.com>
2022-04-25 11:19:20 +0200

    Added kaggle

--- configs/paths.py
+++ configs/paths.py
@@ -1,23 +1,26 @@
 import socket
 import getpass
-
+import os
 username = getpass.getuser()
 
 hostname = socket.gethostname()
-
-if hostname == 'yenchi-pc':
-    dataroot = '/home/yenchi/generative_transformers/data'
-    logroot = '/home/yenchi/generative_transformers/logs'
-elif hostname == 'u110459':
-    dataroot= '/data/datasets/paritosh'
-    logroot= '/data/paritosh/generative_transformers/logs'
-    if username == 'yenchi':
-        dataroot = '/data/yenchi/generative_transformers/data'
-        logroot = '/data/yenchi/generative_transformers/logs'
-elif hostname == 'euclid':
-    if username == 'yenchi':
-        dataroot = '/data/yenchi/generative_transformers/data'
-        logroot = '/data/yenchi/generative_transformers/logs'
+if os.path.isdir('/kaggle/input/mandible-dataset1/'):
+    dataroot = '/kaggle/input/mandible-dataset1/'
+    logroot = 'logs'
 else:
-    dataroot = '..'
-    logroot = '../logs'
+    if hostname == 'yenchi-pc':
+        dataroot = '/home/yenchi/generative_transformers/data'
+        logroot = '/home/yenchi/generative_transformers/logs'
+    elif hostname == 'u110459':
+        dataroot= '/data/datasets/paritosh'
+        logroot= '/data/paritosh/generative_transformers/logs'
+        if username == 'yenchi':
+            dataroot = '/data/yenchi/generative_transformers/data'
+            logroot = '/data/yenchi/generative_transformers/logs'
+    elif hostname == 'euclid':
+        if username == 'yenchi':
+            dataroot = '/data/yenchi/generative_transformers/data'
+            logroot = '/data/yenchi/generative_transformers/logs'
+    else:
+        dataroot = '..'
+        logroot = '../logs'
